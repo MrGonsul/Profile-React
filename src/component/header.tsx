fba6b9b730fff6108bc348fe937d6524ef11d4ca
Mr Gonsul <abdalmohimn68@gmail.com>
2026-09-05 23:09:39 +0200

    fix: update profile image source in header component

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -6,7 +6,7 @@ function Header() {
       <div className="content">
         <div className="container">
           <div className="image">
-            <img src="/DD.jpg" alt="" />
+            <img src="/profile.jpg" alt="" />
           </div>
           <div className="info">
             <h2>Mr Gonsul</h2>
