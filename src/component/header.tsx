37e96c63c77245bb6b48addd64f52347a099fb62
Mr.Gonsul <abdalmohimn68@gmail.com>
2026-09-05 22:23:49 +0200

    Update header.tsx

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -6,7 +6,7 @@ function Header() {
       <div className="content">
         <div className="container">
           <div className="image">
-          <img src="/profile-react/profile.png" alt="Profile" />
+          <img src="public/DD.jpg" alt="Profile" />
           </div>
           <div className="info">
             <h2>Mr Gonsul</h2>
